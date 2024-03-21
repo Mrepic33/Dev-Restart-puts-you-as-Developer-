8d35aaa6292e0b48ff8b839f0b89bee5579c28de
Mrepic33 <162351245+Mrepic33@users.noreply.github.com>
2024-03-21 09:07:15 -0400

    Create Window-NEW!.js

---
+++ Window-NEW!.js
@@ -0,0 +1,14 @@
+window.chrome.start = "stat::log:TGGA''scan + "APP.Start" + "microsoft.store:: Startup" <<<<<startup.stem{stemcode}copy...'con.save.copyed.clip' + """"ADDAF"""">>>>>>
+
+if
+
+404:Eror-- enabled == disable con/cc.error.log {{{{{>>>-(log.port)-<<<}}}}}
+
+else + if
+
+  inspect(cc-file) + (scan Open com.files)( ++ )(local:;;+"hiden.files == show"
+
+con.error.log --- set.DEV((((--'developer//development mode//mode-rep.go+enable'--}.management == /None'none'no management == + delete:chrome-exsention'clear'
+                             if
+                             port:"delete:chrome-exsention'clear' == False 
+  else
