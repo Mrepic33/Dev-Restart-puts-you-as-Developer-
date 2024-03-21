4b7ff9211f4718d9c0651ad1d71ba78084c02116
Mrepic33 <162351245+Mrepic33@users.noreply.github.com>
2024-03-21 08:45:40 -0400

    Create Dev-Mode-Restart.js
    For Chrome OS

---
+++ Dev-Mode-Restart.js
@@ -0,0 +1,19 @@
+window.location.href = "chrome/dev.cc(ARDM)/34753569...-debug(mdc);
+
+if
+  404eror - debug_cc.file - Open.Titaqm.file
+else
+
+chromiumOS.shutdown = 'chrome://quit.devmode.dash/cc-Reset.chrome? == 1.0890.44///uKgmm-enable'
+
+==
+
+if
+  disabled (404:Error==Error:: = signout.management.chrome :: 'Get''chrome://restart/ccdevelopment.mode
+
+else
+
+get"(Chrome.:{{{DEV}}}:.developer mode:{((chrome.'get'-'managedment==(Delete/disable--===--""chromebook""=&=<else--scan.type==compute..--Setting.Typedevelopment.((Window.dismanage.debugg==managedmend//.disable if==complete == "chrome:(debug=management.delete.chrome/.window")))
+
+                                                                                                                                                                                                                                           
+                                                           
